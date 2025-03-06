a447914317a92517c6234032eab1477729431141
FloRul <rumiel.florian@gmail.com>
2025-03-06 15:12:00 -0500

    fix: update output values to use changedetection_instance_ip for consistency

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,9 +1,9 @@
 output "changedetection_instance_ip" {
   description = "Public IP address of the ChangeDetection.io instance"
-  value       = module.cd_lightail_instance.public_ip_address
+  value       = module.cd_lightail_instance.changedetection_instance_ip
 }
 
 output "changedetection_url" {
   description = "URL to access ChangeDetection.io"
-  value       = "http://${module.cd_lightail_instance.public_ip_address}:5000"
+  value       = "http://${module.cd_lightail_instance.changedetection_instance_ip}:5000"
 }
